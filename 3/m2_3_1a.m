705f26effe5972edef35494990cb983cdc087594
zhangyx96 <17888825825@163.com>
2017-03-28 13:39:10 +0800

    task3 info-encryption

--- 3/m2_3_1a.m
+++ 3/m2_3_1a.m
@@ -1,15 +1,15 @@
-%空域隐藏
+%驴脮脫貌脪镁虏脴
 close all;clear all;clc;
 load hall.mat;
 [m,n]=size(hall_gray);
-info=randi([0 1],[m n]);                      %随机生成m*n的0-1信息矩阵
+info=randi([0 1],[m n]);                      %脣忙禄煤脡煤鲁脡m*n碌脛0-1脨脜脧垄戮脴脮贸
 pic_encrypted=double(hall_gray);
 for k=1:m
     for j=1:n
-        pix=double(hall_gray(k,j));           %取出像素点
-        pix_bin=dec2bin(pix);                 %转换成2进制
-        pix_bin(end)=num2str(info(k,j));      %最后一位改成信息位
-        pic_encrypted(k,j)=bin2dec(pix_bin);  %重新转成10进制
+        pix=double(hall_gray(k,j));           %脠隆鲁枚脧帽脣脴碌茫
+        pix_bin=dec2bin(pix);                 %脳陋禄禄鲁脡2陆酶脰脝
+        pix_bin(end)=num2str(info(k,j));      %脳卯潞贸脪禄脦禄赂脛鲁脡脨脜脧垄脦禄
+        pic_encrypted(k,j)=bin2dec(pix_bin);  %脰脴脨脗脳陋鲁脡10陆酶脰脝
     end
 end
 pic_encrypted=uint8(pic_encrypted);
